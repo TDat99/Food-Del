9f38111ede28a8980fdb2606366a6c439663fde0
tiendat49 <buitiendat0409@gmail.com>
2025-10-31 13:50:55 +0700

    update backend: fix route /api/food/add and vercel deploy

---
+++ backend/config/db.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://Tiendat29:tiendat123@cluster0.sas3fpc.mongodb.net/?appName=food-del').then(()=>console.log("DB Connected"));
+}
