789d47c75ca7d938bd850e10a11ffa69f30c0c1a
B.Tien Dat <tiendat299204@gmail.com>
2025-12-03 09:10:58 +0700

    Delete backend/config/db.js

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://Tiendat29:tiendat123@cluster0.sas3fpc.mongodb.net/food-del').then(()=>console.log("DB Connected"));
-}
