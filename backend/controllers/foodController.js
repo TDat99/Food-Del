becc10d8b47f95d4c11482210fb2a83e0a369def
TDat99 <tiendat299204@gmail.com>
2025-12-15 11:03:18 +0700

    Fix formatting issue in removeFood function for improved readability

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -40,7 +40,7 @@ const listFood = async (req, res) => {
 // remove food item
 const removeFood = async (req, res) => {
     try {
-        const food = await foodModel.findById(req.body.id);
+        const food = await foodModel.findById(req.body.id); 
         fs.unlink(`uploads/${food.image}`, () => { })
 
         await foodModel.findByIdAndDelete(req.body.id);
